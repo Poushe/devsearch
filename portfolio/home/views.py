3dd475fd5864c55337f6be142657bb422340ae67
Liza Akter <lizaa6154@gmail.com>
2023-10-03 23:58:29 +0100

    delete portfolio inside main folder

--- portfolio/home/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from projects.models import Project
-
-# Create your views here.
-def Home(request):
-    projects=Project.objects.all()
-    return render(request,'home/home.html',{'projects': projects})
--- portfolio/projects/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Project, Review, Tag, Userm
-
-# Register your models here.
-admin.site.register(Project)
-admin.site.register(Review)
-admin.site.register(Tag)
-admin.site.register(Userm)
